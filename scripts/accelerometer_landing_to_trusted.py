963c8d18c091192069e38ead682e4cadb1614589
Eric Seidler <eric.seidler@gmail.com>
2023-10-04 10:46:16 -0500

    fix: update accelerometer landing to trusted script to go from s3 to s3

--- scripts/accelerometer_landing_to_trusted.py
+++ scripts/accelerometer_landing_to_trusted.py
@@ -13,42 +13,52 @@ job = Job(glueContext)
 job.init(args["JOB_NAME"], args)
 
 # Script generated for node Accelerometer Landing
-AccelerometerLanding_node1696368640588 = glueContext.create_dynamic_frame.from_catalog(
-    database="stedi",
-    table_name="accelerometer_landing",
-    transformation_ctx="AccelerometerLanding_node1696368640588",
+AccelerometerLanding_node1696434129259 = glueContext.create_dynamic_frame.from_options(
+    format_options={"multiline": False},
+    connection_type="s3",
+    format="json",
+    connection_options={
+        "paths": ["s3://erics-stedi-lakehouse/accelerometer/landing/"],
+        "recurse": True,
+    },
+    transformation_ctx="AccelerometerLanding_node1696434129259",
 )
 
-# Script generated for node Customer Trusted Zone
-CustomerTrustedZone_node1696367716420 = glueContext.create_dynamic_frame.from_catalog(
-    database="stedi",
-    table_name="customer_trusted",
-    transformation_ctx="CustomerTrustedZone_node1696367716420",
+# Script generated for node Customer Trusted
+CustomerTrusted_node1696434163725 = glueContext.create_dynamic_frame.from_options(
+    format_options={"multiline": False},
+    connection_type="s3",
+    format="json",
+    connection_options={
+        "paths": ["s3://erics-stedi-lakehouse/customer/trusted/"],
+        "recurse": True,
+    },
+    transformation_ctx="CustomerTrusted_node1696434163725",
 )
 
-# Script generated for node Join Customer
-JoinCustomer_node1696368252471 = Join.apply(
-    frame1=CustomerTrustedZone_node1696367716420,
-    frame2=AccelerometerLanding_node1696368640588,
-    keys1=["email"],
-    keys2=["user"],
-    transformation_ctx="JoinCustomer_node1696368252471",
+# Script generated for node Privacy Filter
+PrivacyFilter_node1696368252471 = Join.apply(
+    frame1=AccelerometerLanding_node1696434129259,
+    frame2=CustomerTrusted_node1696434163725,
+    keys1=["user"],
+    keys2=["email"],
+    transformation_ctx="PrivacyFilter_node1696368252471",
 )
 
 # Script generated for node Drop Fields
 DropFields_node1696368405404 = DropFields.apply(
-    frame=JoinCustomer_node1696368252471,
+    frame=PrivacyFilter_node1696368252471,
     paths=[
-        "customername",
         "email",
         "phone",
-        "birthday",
-        "serialnumber",
-        "registrationdate",
-        "lastupdatedate",
-        "sharewithresearchasofdate",
-        "sharewithpublicasofdate",
-        "sharewithfriendsasofdate",
+        "shareWithFriendsAsOfDate",
+        "lastUpdateDate",
+        "shareWithResearchAsOfDate",
+        "registrationDate",
+        "customerName",
+        "birthDay",
+        "shareWithPublicAsOfDate",
+        "serialNumber",
     ],
     transformation_ctx="DropFields_node1696368405404",
 )
