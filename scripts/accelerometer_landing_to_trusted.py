9433883ed3c3c953ef5bca4d5f7bdab0a8200e10
Eric Seidler <eric.seidler@gmail.com>
2023-10-04 09:36:18 -0500

    new: add glue job scripts for creating accelerometer and customer trusted

---
+++ scripts/accelerometer_landing_to_trusted.py
@@ -0,0 +1,67 @@
+import sys
+from awsglue.transforms import *
+from awsglue.utils import getResolvedOptions
+from pyspark.context import SparkContext
+from awsglue.context import GlueContext
+from awsglue.job import Job
+
+args = getResolvedOptions(sys.argv, ["JOB_NAME"])
+sc = SparkContext()
+glueContext = GlueContext(sc)
+spark = glueContext.spark_session
+job = Job(glueContext)
+job.init(args["JOB_NAME"], args)
+
+# Script generated for node Accelerometer Landing
+AccelerometerLanding_node1696368640588 = glueContext.create_dynamic_frame.from_catalog(
+    database="stedi",
+    table_name="accelerometer_landing",
+    transformation_ctx="AccelerometerLanding_node1696368640588",
+)
+
+# Script generated for node Customer Trusted Zone
+CustomerTrustedZone_node1696367716420 = glueContext.create_dynamic_frame.from_catalog(
+    database="stedi",
+    table_name="customer_trusted",
+    transformation_ctx="CustomerTrustedZone_node1696367716420",
+)
+
+# Script generated for node Join Customer
+JoinCustomer_node1696368252471 = Join.apply(
+    frame1=CustomerTrustedZone_node1696367716420,
+    frame2=AccelerometerLanding_node1696368640588,
+    keys1=["email"],
+    keys2=["user"],
+    transformation_ctx="JoinCustomer_node1696368252471",
+)
+
+# Script generated for node Drop Fields
+DropFields_node1696368405404 = DropFields.apply(
+    frame=JoinCustomer_node1696368252471,
+    paths=[
+        "customername",
+        "email",
+        "phone",
+        "birthday",
+        "serialnumber",
+        "registrationdate",
+        "lastupdatedate",
+        "sharewithresearchasofdate",
+        "sharewithpublicasofdate",
+    ],
+    transformation_ctx="DropFields_node1696368405404",
+)
+
+# Script generated for node Accelerometer Trusted
+AccelerometerTrusted_node2 = glueContext.write_dynamic_frame.from_options(
+    frame=DropFields_node1696368405404,
+    connection_type="s3",
+    format="json",
+    connection_options={
+        "path": "s3://erics-stedi-lakehouse/accelerometer/trusted/",
+        "partitionKeys": [],
+    },
+    transformation_ctx="AccelerometerTrusted_node2",
+)
+
+job.commit()
---
+++ scripts/customer_landing_to_trusted.py
@@ -0,0 +1,49 @@
+import sys
+from awsglue.transforms import *
+from awsglue.utils import getResolvedOptions
+from pyspark.context import SparkContext
+from awsglue.context import GlueContext
+from awsglue.job import Job
+import re
+
+args = getResolvedOptions(sys.argv, ["JOB_NAME"])
+sc = SparkContext()
+glueContext = GlueContext(sc)
+spark = glueContext.spark_session
+job = Job(glueContext)
+job.init(args["JOB_NAME"], args)
+
+# Script generated for node customer landing
+customerlanding_node1 = glueContext.create_dynamic_frame.from_options(
+    format_options={"multiline": False},
+    connection_type="s3",
+    format="json",
+    connection_options={
+        "paths": [
+            "s3://erics-stedi-lakehouse/customer/landing/customer-keep-1655293787679.json"
+        ],
+        "recurse": True,
+    },
+    transformation_ctx="customerlanding_node1",
+)
+
+# Script generated for node Filter
+Filter_node1696363744346 = Filter.apply(
+    frame=customerlanding_node1,
+    f=lambda row: (not (row["shareWithResearchAsOfDate"] == 0)),
+    transformation_ctx="Filter_node1696363744346",
+)
+
+# Script generated for node Trusted Customer Zone
+TrustedCustomerZone_node2 = glueContext.write_dynamic_frame.from_options(
+    frame=Filter_node1696363744346,
+    connection_type="s3",
+    format="json",
+    connection_options={
+        "path": "s3://erics-stedi-lakehouse/customer/trusted/",
+        "partitionKeys": [],
+    },
+    transformation_ctx="TrustedCustomerZone_node2",
+)
+
+job.commit()
